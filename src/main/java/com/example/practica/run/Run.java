bf991956f7c156520292dd6f41b437b2cbd7c6ab
ronalmoctz <moctz23@outlook.com>
2024-12-17 12:28:43 -0600

    add post, put and delte API

--- src/main/java/com/example/practica/run/Run.java
+++ src/main/java/com/example/practica/run/Run.java
@@ -3,10 +3,16 @@ package com.example.practica.run;
 import java.time.LocalDateTime;
 
 public record Run(
-        Integer id,
-        String title,
-        LocalDateTime startedOn,
-        LocalDateTime CompleteOn,
-        Integer miles,
-        Location location) {
+                Integer id,
+                String title,
+                LocalDateTime startedOn,
+                LocalDateTime completedOn,
+                Integer miles,
+                Location location) {
+
+        public Run {
+                if (!completedOn.isAfter(startedOn)) {
+                        throw new IllegalArgumentException("Completed on cannot be before started on");
+                }
+        }
 }
--- src/main/java/com/example/practica/run/RunRespository.java
+++ src/main/java/com/example/practica/run/RunRespository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -17,8 +18,27 @@ public class RunRespository {
                 return runs;
         }
 
-        Run findById(Integer id) {
-                return runs.stream().filter(run -> run.id() == id).findFirst().get();
+        Optional<Run> findById(Integer id) {
+                return runs.stream().filter(run -> run.id() == id).findFirst();
+        }
+
+        // Se crea nuevo arguemento para un nuevo corredor
+        // el cual es inmutable
+        void create(Run run) {
+                runs.add(run);
+        }
+
+        // reliza una busqueda, primero si existe la run si no existe
+        // finaza, si existe continua y actualiza la run
+        void update(Run run, Integer id) {
+                Optional<Run> existingRun = findById(id);
+                if (existingRun.isPresent()) {
+                        runs.set(runs.indexOf(existingRun.get()), run);
+                }
+        }
+
+        void delete(Integer id) {
+                runs.removeIf(run -> run.id() == id);
         }
 
         @PostConstruct
